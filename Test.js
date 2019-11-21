b7abf174b0e86c53110c3766751a443fcba598e1
DEVDESK <jesse.cruz.wright@gmail.com>
2019-11-21 18:59:35 +0000

    up

--- Test.js
+++ Test.js
@@ -1,10 +1,19 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const enmap = require("enmap");
+const fs = require("fs"	);
 //const { StatsD } = require("hot-shots");
 //client.dStats = new StatsD("localhost", 8125);
+const nsfwjs = require("nsfw-node");
+const img = fs.readFileSync("./data/test.jpg");
 
+nsfwjs.NsfwNode.load().then(function(model) {
+	model.classify(img).then(function(predictions) {
+		console.log("Predictions: ", predictions);
+	});
+});
 
+/*
 client.DB = new enmap({
 	name: "DB",
 	autoFetch: true,
@@ -23,3 +32,4 @@ client.on("ready", () => {
 	client.DB.defer.then(client.init(client));
 });
 
+*/
