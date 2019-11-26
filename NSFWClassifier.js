716c854641e9aa80a3187afbed8131f3586f3690
DEVDESK <jesse.cruz.wright@gmail.com>
2019-11-26 18:39:56 +0000

    NSFW Classifier Operational!

--- NSFWClassifier.js
+++ NSFWClassifier.js
@@ -1,30 +1,25 @@
 const tf = require("@tensorflow/tfjs-node");
 const load = require("nsfwjs").load;
 const fs = require("fs");
-const imageGet = require("get-image-data");
-const { Image, createCanvas } = require("canvas");
 //to future me: check if the ImageByteArray can be done through get-image-data or not. if it can, use it!!
 
-const loadLocalImage = async (path) => {
-	image(path, function (err, info) {
-		if (err) console.log(err)
-		info.data = Uint8Array.from(info.data)
-		return (tf.node.decodeImage(info.data, undefined, undefined, false))
-	});
-
-};
+const readImage = path => {
+	const imageBuffer = fs.readFileSync(path);
+	const tfimage = tf.node.decodeImage(imageBuffer, undefined, undefined, false);
+	return tfimage;
+}
 
 load("file://./model/").then(model => {
 	fs.readdir("./cache", (err, images) => {
 		if (err) console.log(err);
 		images.forEach(img => {
-			loadLocalImage(`./cache/${img}`).then(pimg => {
-				model.classify(pimg).then(predictions => {
-					console.log(JSON.stringify(predictions));
-				});
+			var image = readImage(`./cache/${img}`)
+			model.classify(image).then(predictions => {
+				console.log(JSON.stringify(predictions));
 			});
 		});
 	});
 });
+
 
 
