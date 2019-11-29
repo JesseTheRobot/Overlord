01b76d790b1a20936129880ae6ce5395ee734de9
DEVDESK <jesse.cruz.wright@gmail.com>
2019-11-29 10:06:47 +0000

    integrate NSFWtest sections

---
+++ NSFWtestor/NSFWClassifier.js
@@ -0,0 +1,22 @@
+const tf = require("@tensorflow/tfjs-node");
+const load = require("nsfwjs").load;
+const fs = require("fs");
+
+const readImage = path => {
+	const imageBuffer = fs.readFileSync(path);
+	const tfimage = tf.node.decodeImage(imageBuffer, undefined, undefined, false);
+	return tfimage;
+}
+
+load("file://./model/").then(model => {
+	fs.readdir("./cache", (err, images) => {
+		if (err) console.log(err);
+		images.forEach(img => {
+			var image = readImage(`./cache/${img}`)
+			model.classify(image, 3).then(predictions => {
+				console.log(JSON.stringify(predictions))
+				console.log(`${img}: ${predictions[0].className} with probability ${predictions[0].probability}`);
+			}).catch(err)
+		});
+	});
+});
---
+++ NSFWtestor/Overlord.js
@@ -0,0 +1,57 @@
+﻿/**Dependancy Import and initialisation */
+const Discord = require("discord.js");
+const tf = require("@tensorflow/tfjs-node");
+const load = require("nsfwjs").load;
+const fs = require("fs");
+const client = new Discord.Client({ autoReconnect: true, messageCacheMaxSize: -1, messageCacheLifetime: 0, messageSweepInterval: 0, fetchAllMembers: true });
+client.download = require("download-file");
+
+
+const readImage = async (path) => {
+	const imageBuffer = fs.readFileSync(path);
+	const tfimage = tf.node.decodeImage(imageBuffer, undefined, undefined, false);
+	return tfimage;
+}
+
+client.on("ready", () => {
+	console.log("ready!")
+})
+var initmodel = async (client) => {
+	client.model = await load("file://./model/")
+}
+
+initmodel(client).then(() => {
+	client.login("NjQ4OTU5OTU5NDg4Mzk3MzMy.Xd11Kw.dHib7KEW6nczwGqMs3GUAWmNb3g");
+})
+
+
+var classify = async (client, img) => {
+	readImage(`./cache/${img}`).then(image => {
+		client.model.classify(image, 3).then(predictions => {
+			console.log(JSON.stringify(predictions))
+			return predictions
+			//return (`${img}: ${predictions[0].className} with probability ${predictions[0].probability}`)
+		}).catch(function (err) { console.log(err); });
+	})
+}
+
+
+client.on("message", (message) => {
+	console.log(message.content)
+	console.log(message)
+	message.attachments.array().forEach(att => {
+		var fname = message.id + "." + att.url.split("/").pop().split(".")[1]
+		client.download(att.url, { directory: "./cache", filename: fname }, function (err) {
+			if (err) client.log("ERROR", `download of attachment ${att.url} failed!`, "recordAttachments");
+			else {
+				console.log("download successful!");
+				console.log(fname)
+				classify(client, fname).then(response => {
+					console.log(response)
+					message.reply(`results: ${JSON.stringify(response)}`)
+				})
+
+			}
+		})
+	})
+})
