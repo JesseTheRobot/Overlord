435c9fd906b0ab88a9863a062b03684ad67acd6f
DEVDESK <jesse.cruz.wright@gmail.com>
2020-03-13 10:20:38 +0000

    up

--- events/autoMod.js
+++ events/autoMod.js
@@ -3,7 +3,7 @@ module.exports = async (client, message) => {
     let modConfig = config.autoMod
     let antiSpam = async (client, message) => {
         let member = message.member
-        let trecent = client.trecent.get(message.guild.id)
+        let trecent = client.trecent
         if (Array.from(member.roles).filter(role => config.excludedRoles.includes(role)).size >= 1) { return } //exclude those who have configured 'protected' roles
         trecent.ensure(message.guild.id, [], message.channel.id)
         setTimeout(() => { trecent.remove(message.guild.id, message.channel.id, member.id) }, modConfig.interval)
@@ -11,47 +11,34 @@ module.exports = async (client, message) => {
         if ((trecent.get(message.guild.id, message.channel.id).filter((user) => user === member.id)).length >= modConfig.count) {
             client.log("exceeded")
         }
-        //wipes array after a strike has been added.
-        if (!user.strikes) {
-            user.strikes = [];
-        }
-        user.strikes.push(new Date());
-        message.channel.send(`${message.author} has had a strike added!`);
-        //penalise the user!
-    } else {
-        trecent.push(mobj);
-}
-    }
-let antiMention = (client, message) => {
-    if (message.mentions.array.filter(mentionedID => modConfig.antiMention.has(mentionedID)).length >= 1) {
-        //penalise the user!
-    }
-}
 
-}
-module.exports.defaultConfig = {
-    enabled: true,
-    bannedWords: [],
-    excludedRoles: [],
-    percentCapsLimit: 0,
-    floodPercentLimit: 0,
-    decay: 30000,
-    antiSpam: {
+    }
+    let penalise = async (client, user, data) => {
+
+    }
+    module.exports.defaultConfig = {
         enabled: true,
-        interval: 2000,
-        count: 2,
-        penalty: 1,
-    },
-    penalties: {
-        repeatOffenceMultiplier: 2,
-        repeatOffenceTimeout: 10000
-    },
-    punishments: {
-        5: "mute",
-        10: "tempBan",
-        15: "ban",
-    },
-    requiredPermissions: ["MANAGE_MESSAGES"]
+        bannedWords: [],
+        excludedRoles: [],
+        percentCapsLimit: 0,
+        floodPercentLimit: 0,
+        decay: 30000,
+        antiSpam: {
+            enabled: true,
+            interval: 2000,
+            count: 2,
+            penalty: 1,
+        },
+        penalties: {
+            repeatOffenceMultiplier: 2,
+            repeatOffenceTimeout: 10000
+        },
+        punishments: {
+            5: "mute",
+            10: "tempBan",
+            15: "ban",
+        },
+        requiredPermissions: ["MANAGE_MESSAGES"]
 
-}
+    }
 
