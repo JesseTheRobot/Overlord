282d4c3be75ae7f95802f4a55308ff6ac8ee1917
DEVDESK <jesse.cruz.wright@gmail.com>
2020-03-13 00:19:15 +0000

    up

--- events/autoMod.js
+++ events/autoMod.js
@@ -4,27 +4,29 @@ module.exports = async (client, message) => {
     let antiSpam = async (client, message) => {
         let member = message.member
         let trecent = client.trecent.get(message.guild.id)
-        if (member.roles.array.filter(role => config.excludedRoles.has(role)).size >= 1) { return } //exclude those who have configured 'protected' roles
-        trecent.ensure([], message.channel.id)
-        trecent.push(message.channel.id,member.id,true) //remember to transition to local scoping!
-        setTimeout(()=>{trecent.remove(message.channel.id,member.id)},modConfig.interval)
-        if((trecent.filterArray((user) =>user === member.id)).length >= modConfig.count){ //filter all messages sent (within array) and if <mutecap> or more are keyed to the user and guildid, penalise the user. 
-            //wipes array after a strike has been added.
-            if (!user.strikes) {
-                user.strikes = [];
-            }
-            user.strikes.push(new Date());
-            message.channel.send(`${message.author} has had a strike added!`);
-            //penalise the user!
-        } else {
-            trecent.push(mobj);
+        if (Array.from(member.roles).filter(role => config.excludedRoles.includes(role)).size >= 1) { return } //exclude those who have configured 'protected' roles
+        trecent.ensure(message.guild.id, [], message.channel.id)
+        setTimeout(() => { trecent.remove(message.guild.id, message.channel.id, member.id) }, modConfig.interval)
+        setTimeout(() => { client.log(trecent) }, 5000)
+        if ((trecent.get(message.guild.id, message.channel.id).filter((user) => user === member.id)).length >= modConfig.count) {
+            client.log("exceeded")
         }
-    }
-    let antiMention = (client, message) => {
-        if (message.mentions.array.filter(mentionedID => modConfig.antiMention.has(mentionedID)).length >= 1) {
-            //penalise the user!
+        //wipes array after a strike has been added.
+        if (!user.strikes) {
+            user.strikes = [];
         }
+        user.strikes.push(new Date());
+        message.channel.send(`${message.author} has had a strike added!`);
+        //penalise the user!
+    } else {
+        trecent.push(mobj);
+}
     }
+let antiMention = (client, message) => {
+    if (message.mentions.array.filter(mentionedID => modConfig.antiMention.has(mentionedID)).length >= 1) {
+        //penalise the user!
+    }
+}
 
 }
 module.exports.defaultConfig = {
